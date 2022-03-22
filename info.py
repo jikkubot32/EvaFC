c3728ebb11ffad7cda49c795874f287f123640ef
jikkubot32 <93936137+jikkubot32@users.noreply.github.com>
2022-03-22 18:41:48 +0400

    Update info.py

--- info.py
+++ info.py
@@ -12,41 +12,76 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ['API_ID'])
-API_HASH = environ['API_HASH']
-BOT_TOKEN = environ['BOT_TOKEN']
+API_ID = int(environ['3937220'])
+API_HASH = environ['a4de4c3e683bfdf30b5781142e0d41f2']
+BOT_TOKEN = environ['5096921899:AAHN8eExI_fhmzWY7cD-7tM_kX58u8QoP8o']
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
-USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
-PICS = (environ.get('PICS', 'https://telegra.ph/file/8b42f6caf6ef5fd76766f.jpg https://telegra.ph/file/82b5bbbab6d5e5593b6b2.jpg')).split()
+USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
+PICS = (environ.get('PICS', 'https://telegra.ph/file/c774cca7f44d3464ae76b.jpg
+
+https://telegra.ph/file/79f50dfea4f89d8604c66.jpg
+
+https://telegra.ph/file/86d6169df8c962443eeef.jpg
+
+https://telegra.ph/file/729d2a137e53421078b74.jpg
+
+https://telegra.ph/file/7f04250a4942fa40b6e68.jpg
+
+https://telegra.ph/file/fe22c628729a025703508.jpg
+
+https://telegra.ph/file/992034ee054d01d25065b.jpg
+
+https://telegra.ph/file/efc96314636379b2e6567.jpg
+
+https://telegra.ph/file/30505152d21d586835655.jpg
+
+https://telegra.ph/file/0013942adb9ec0c73617a.jpg
+https://telegra.ph/file/d8dd013ce917cf429a015.jpg https://telegra.ph/file/e02a8c7453d0f375be101.jpg https://telegra.ph/file/d3ada756d0c874aa73e95.jpg https://telegra.ph/file/4f1743c359b75799be337.jpg https://telegra.ph/file/5e243a1a709156e0260d1.jpg https://telegra.ph/file/e6e37eeb762d114c8f5a0.jpg https://telegra.ph/file/489d90e0526b29e47a214.jpg https://telegra.ph/file/bc0c6d6a5733272bc7bd8.jpg https://telegra.ph/file/d65a01adc67ac25ec0cb1.jpg https://telegra.ph/file/0f0284b60638d98eb5d50.jpg')).split()
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '0').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '658905997').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001484884182 -1001172622040 -1001280478789').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
-auth_channel = environ.get('AUTH_CHANNEL')
+auth_channel = environ.get('-1001157840306')
 auth_grp = environ.get('AUTH_GROUP')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Rajappan")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Telegram:Telegram@cluster0.ahnw6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Telegram")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', 0))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', -1001501315486))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'FilmClubChannel')
-P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
+P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "True")), False)
 IMDB = is_enabled((environ.get('IMDB', "True")), True)
-SINGLE_BUTTON = is_enabled((environ.get('SINGLE_BUTTON', "False")), False)
-CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", None)
-IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", "<b>Query: {query}</b> \n‌‌‌‌IMDb Data:\n\n🏷 Title: <a href={url}>{title}</a>\n🎭 Genres: {genres}\n📆 Year: <a href={url}/releaseinfo>{year}</a>\n🌟 Rating: <a href={url}/ratings>{rating}</a> / 10")
+SINGLE_BUTTON = is_enabled((environ.get('SINGLE_BUTTON', "True")), False)
+CUSTOM_FILE_CAPTION = environ.get("<code>{file_name}</code>
+
+<b>Size: <code>{file_size}</code></b>
+
+<b>© Powered by [FilmClubChannel](https://t.me/FilmClubChannel) </b>", None)
+IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", "<b>🏷 Title</b>: <a href={url}>{title}</a>
+🎭 Genres: {genres}
+📆 Year: <a href={url}/releaseinfo>{year}</a>
+🌟 Rating: <a href={url}/ratings>{rating}</a> / 10 (based on {votes} user ratings.)
+☀️ Languages : <code>{languages}</code>
+👥 Cast : <code>{cast}</code>
+📀 RunTime: {runtime} Minutes
+📆 Release Info : {release_date}
+🎛 Countries : <code>{countries}</code>
+👥 Requested By :  {message.from_user.mention}
+🌐 Channel : @FilmClubChannel
+
+© {message.chat.title} </b>")
 LONG_IMDB_DESCRIPTION = is_enabled(environ.get("LONG_IMDB_DESCRIPTION", "False"), False)
 SPELL_CHECK_REPLY = is_enabled(environ.get("SPELL_CHECK_REPLY", "True"), True)
-MAX_LIST_ELM = environ.get("MAX_LIST_ELM", None)
+MAX_LIST_ELM = environ.get("4", None)
 INDEX_REQ_CHANNEL = int(environ.get('INDEX_REQ_CHANNEL', LOG_CHANNEL))
 
 LOG_STR = "Current Cusomized Configurations are:-\n"
