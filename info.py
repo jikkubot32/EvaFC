b045802fef4ffe43402ecb31d99505af2d2d5609
jikkubot32 <93936137+jikkubot32@users.noreply.github.com>
2022-03-22 22:01:22 +0400

    Update info.py

--- info.py
+++ info.py
@@ -26,24 +26,24 @@ ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001484884182 -1001172622040 -1001280478789').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
-auth_channel = environ.get('-1001157840306')
+auth_channel = environ.get('AUTH_CHANNEL')
 auth_grp = environ.get('AUTH_GROUP')
-AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
+AUTH_CHANNEL = -1001157840306
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Telegram:Telegram@cluster0.ahnw6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Telegram")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
+DATABASE_URI = "mongodb+srv://Telegram:Telegram@cluster0.ahnw6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+DATABASE_NAME = "Telegram"
+COLLECTION_NAME = 'Telegram_files'
 
 # Others
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', -1001501315486))
+LOG_CHANNEL = -1001501315486
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'FilmClubChannel')
-P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "True")), False)
-IMDB = is_enabled((environ.get('IMDB', "True")), True)
-SINGLE_BUTTON = is_enabled((environ.get('SINGLE_BUTTON', "True")), False)
+P_TTI_SHOW_OFF = "True"
+IMDB = "True"
+SINGLE_BUTTON = "True"
 CUSTOM_FILE_CAPTION = "<code>{file_name}</code>\n\n<b>Size: <code>{file_size}</code></b>\n\n<b>© Powered by [FilmClubChannel](https://t.me/FilmClubChannel) </b>" 
-IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", "<b>🏷 Title</b>: <a href={url}>{title}</a>\n🎭 Genres: {genres}\n📆 Year: <a href={url}/releaseinfo>{year}</a>\n🌟 Rating: <a href={url}/ratings>{rating}</a> / 10 (based on {votes} user ratings.)\n☀️ Languages : <code>{languages}</code>\n👥 Cast : <code>{cast}</code>\n📀 RunTime: {runtime} Minutes\n📆 Release Info : {release_date}\n🎛 Countries : <code>{countries}</code>\n👥 Requested By :  {message.from_user.mention}\n🌐 Channel : @FilmClubChannel\n\n© {message.chat.title} </b>")
+IMDB_TEMPLATE = "<b>🏷 Title</b>: <a href={url}>{title}</a>\n🎭 Genres: {genres}\n📆 Year: <a href={url}/releaseinfo>{year}</a>\n🌟 Rating: <a href={url}/ratings>{rating}</a> / 10 (based on {votes} user ratings.)\n☀️ Languages : <code>{languages}</code>\n👥 Cast : <code>{cast}</code>\n📀 RunTime: {runtime} Minutes\n📆 Release Info : {release_date}\n🎛 Countries : <code>{countries}</code>\n👥 Requested By :  {message.from_user.mention}\n🌐 Channel : @FilmClubChannel\n\n© {message.chat.title} </b>"
 LONG_IMDB_DESCRIPTION = is_enabled(environ.get("LONG_IMDB_DESCRIPTION", "False"), False)
 SPELL_CHECK_REPLY = is_enabled(environ.get("SPELL_CHECK_REPLY", "True"), True)
 MAX_LIST_ELM = environ.get("4", None)
